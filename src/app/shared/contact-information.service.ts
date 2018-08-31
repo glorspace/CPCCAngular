59959505599bf5719c441163b651c34254e32789
glorspace <tommysims@gmail.com>
2018-08-31 14:45:39 -0400

    Added the IMailingAddress interface and service methods in ContactInformationService to return contact information.

--- src/app/shared/contact-information.service.ts
+++ src/app/shared/contact-information.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IMailingAddress } from './mailing-address';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,22 @@ import { Injectable } from '@angular/core';
 export class ContactInformationService {
 
   constructor() { }
+
+  getMailingAddress(): IMailingAddress {
+    return {
+      addressLine1: 'P.O. Box 84458',
+      addressLine2: '',
+      city: 'Lexington',
+      stateCode: 'SC',
+      zipCode: '29073'
+    }
+  }
+
+  getPhoneNumber(): string {
+    return '803 359-6106';
+  }
+
+  getEmailAddress(): string {
+    return 'info@centerpointesc.com';
+  }
 }
---
+++ src/app/shared/mailing-address.ts
@@ -0,0 +1,7 @@
+export interface IMailingAddress {
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    stateCode: string;
+    zipCode: string;
+}
